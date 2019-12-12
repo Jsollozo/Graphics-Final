b3ba49ac6978aea396993ffa31d51b4cdf0b9591
DragonPickels <jennyrodriguez843@gmail.com>
2019-12-11 23:27:49 -0800

    Sunlight Finsihed

--- codebase/final.js
+++ codebase/final.js
@@ -96,6 +96,7 @@ function animate() {
 }
 
 function world(){
+  sun();
   /*World Plane*/
   //Road
   installModel(objects[1]);
@@ -123,7 +124,7 @@ function world(){
 
   tree();
   car();
-  sun();
+  //sun();
   streetLight();
 }
 
@@ -397,17 +398,18 @@ function car(){
 function sun(){
   installModel(objects[2]);
   currentModelNumber = 2;
-  var sunlight = [ modelview[12], modelview[13], modelview[14] ];
   mat4.translate(modelview, modelview, [1, 3, 1, 1]);
-  if(daytime){
-    gl.uniform4f(u_diffuseColor, 0.7 ,0.7, 0, 1);
+  
+  var sunlight = [ modelview[12], modelview[13], modelview[14] ];
+  lightPositions[0] = sunlight;
+  if(day){
+    gl.uniform4f(u_diffuseColor, 1 ,1, 0, 1);
     gl.uniform4f(u_lightPosition, sunlight[0], sunlight[1], sunlight[2]+2, 1);
   }
   else{
     //gl.uniform4fv(u_lightPosition, [0, 10, 0, 0]);
     gl.uniform4f(u_diffuseColor, 0.7 ,0.7, 0, 1);
   }
-
   update_uniform(modelview, projection, 2);
   modelview = rotator.getViewMatrix();
 
@@ -462,7 +464,7 @@ function streetLight(){
   //light
   installModel(objects[2]);
   currentModelNumber = 2;
-  if(!daytime){
+  if(!day){
     //set light pos for night time
     //gl.uniform4fv(u_lightPosition, lightPositions[1]);
     gl.uniform4f(u_diffuseColor, 1, 1, 0, 1);
@@ -536,7 +538,7 @@ function initGL(){
   gl.uniform4f(u_diffuseColor, 1, 1, 1, 1);
   gl.uniform1f(u_specularExponent, 10);
   //gl.uniform4f(u_lightPosition, lightPositions[1][0], lightPositions[1][1],lightPositions[0][2], lightPositions[1][3]);
-  //gl.uniform4f(u_lightPosition, 0, 0, 0, 1);
+  //gl.uniform4f(u_lightPosition, lightPositions[0][0], lightPositions[0][1], lightPositions[0][2], lightPositions[0][3]);
 }
 
 
